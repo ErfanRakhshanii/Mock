9c78a8a44113682b1c8fd67a137a8e914b846487
Erfan Rakhshani <143809762+ErfanRakhshanii@users.noreply.github.com>
2024-02-24 17:43:54 +0330

    first commit

---
+++ Get/mockGetParamsOnAny.js
@@ -0,0 +1,104 @@
+const axios = require('axios');
+const MockAdapter = require('axios-mock-adapter');
+
+const axiosInstance = axios.create();
+const mock = new MockAdapter(axiosInstance, { delayResponse: 2000 });
+
+const users = [
+    {
+        "user_id": "583c3ac3f38e84297c002546",
+        "email": "test@test.com",
+        "name": "test@test.com",
+        "given_name": "Hello",
+        "family_name": "Test",
+        "nickname": "test",
+        "last_ip": "94.121.163.63",
+        "logins_count": 15,
+        "created_at": "2016-11-28T14:10:11.338Z",
+        "updated_at": "2016-12-02T01:17:29.310Z",
+        "last_login": "2016-12-02T01:17:29.310Z",
+        "email_verified": true
+    },
+    {
+        "user_id": "583c5484cb79a5fe593425a9",
+        "email": "test1@test.com",
+        "name": "test1@test.com",
+        "given_name": "Hello1",
+        "family_name": "Test1",
+        "nickname": "test1",
+        "last_ip": "94.121.168.53",
+        "logins_count": 1,
+        "created_at": "2016-11-28T16:00:04.209Z",
+        "updated_at": "2016-11-28T16:00:47.203Z",
+        "last_login": "2016-11-28T16:00:47.203Z",
+        "email_verified": true
+    },
+    {
+        "user_id": "583c57672c7686377d2f66c9",
+        "email": "aaa@aaa.com",
+        "name": "aaa@aaa.com",
+        "given_name": "John",
+        "family_name": "Dough",
+        "nickname": "aaa",
+        "last_ip": "94.121.168.53",
+        "logins_count": 2,
+        "created_at": "2016-11-28T16:12:23.777Z",
+        "updated_at": "2016-11-28T16:12:52.353Z",
+        "last_login": "2016-11-28T16:12:52.353Z",
+        "email_verified": true
+    },
+    {
+        "user_id": "5840b954da0529cd293d76fe",
+        "email": "a@a.com",
+        "name": "a@a.com",
+        "given_name": "Jane",
+        "family_name": "Dough",
+        "nickname": "a",
+        "last_ip": "94.121.163.63",
+        "logins_count": 3,
+        "created_at": "2016-12-01T23:59:16.473Z",
+        "updated_at": "2016-12-01T23:59:53.474Z",
+        "last_login": "2016-12-01T23:59:53.474Z",
+        "email_verified": true
+    },
+    {
+        "user_id": "584a9d13e808bcf75f05f580",
+        "email": "test9999@test.com",
+        "given_name": "Dummy",
+        "family_name": "User",
+        "created_at": "2016-12-09T12:01:23.787Z",
+        "updated_at": "2016-12-09T12:01:23.787Z",
+        "email_verified": false
+    }
+];
+
+mock.onAny().reply(config => {
+    const url = config.url;
+    const method = config.method;
+    const { params } = config;
+    
+    if (method === 'GET' && url === '/users') {
+        const { family_name } = params;
+
+        if (!family_name) {
+            return [400, 'پارامتر family_name الزامی است.'];
+        }
+
+        const filteredUsers = users.filter(user => user.family_name === family_name);
+
+        return [200, filteredUsers];
+    }
+
+    return [404, 'صفحه مورد نظر یافت نشد.'];
+});
+
+axiosInstance.get('/users', { params: { family_name: 'Dough' } })
+    .then(response => {
+        console.log('کاربران با نام خانوادگی Dough:', response.data);
+    })
+    .catch(error => {
+        console.error('خطا در دریافت اطلاعات کاربران:', error);
+    });
+
+mock.resetHandlers();
+mock.reset();
---
+++ README.md
@@ -0,0 +1,46 @@
+### Description
+In this repository, we have two folders: Post and Get, where various scenarios and relatively complete mock axios implementations are implemented.
+Only post and get methods are considered in this project.
+
+### How to Run
+1. **Install Packages**: First, you need to install the project dependencies.
+   npm install axios axios-mock-adapter
+
+2. **Run the Code**: To run each project, use a command similar to the following:
+   node .\Post\mockPost.js
+   node .\Get\mockGet.js
+
+### File Contents
+#### README.md
+This file contains project guidelines.
+
+#### Post Folder
+This folder contains JavaScript projects with post methods implemented in mock.
+
+#### Get Folder
+This folder contains JavaScript projects with get methods implemented in mock.
+
+### Important Notes
+We use axios-mock-adapter to simulate HTTP requests to ensure that our programs function correctly without sending and receiving from a real service.
+
+### توضیحات
+در اینجا دو فولدر Post و Get داریم که حالت های مختلف و نسبتا تکمیل mock axios در آنها پیاده سازی شده اند
+در این پروژه فقط حالت های post و get در نظر گرفته شده اند
+### نحوه اجرا
+1. **نصب پکیج ها**: برای اجرا ابتدا باید پکیج های پروژه را نصب کنید
+   npm install axios axios-mock-adapter
+
+2. **اجرای کد**: برای اجرای هرکدام از پروژه ها از کامند مشابه زیر استفاده کنید
+   node .\Post\mockPost.js
+   node .\Get\mockGet.js
+
+### محتوای فایل‌ها
+#### README.md
+این فایل شامل راهنماهای پروژه میباشد 
+#### Post Folder
+این فولدر شامل پروژه های جاوااسکریپت با متد های post پیاده سازی در mock میباشند
+#### Get Folder
+این فولدر شامل پروژه های جاوااسکریپت با متد های get پیاده سازی در mock میباشند
+
+### نکات مهم
+ما از axios-mock-adapter برای شبیه‌سازی درخواست‌های HTTP استفاده می‌کنیم تا بتوانیم بدون ارسال و دریافت از یک سرویس واقعی، اطمینان حاصل کنیم که برنامه‌هایمان به درستی عمل می‌کنند.
